72770d4846260dd1431827f1016effa092656532
Görkem Bilim <gorkemblm@gmail.com>
2021-12-26 19:04:57 +0300

    added-two-children-class-for-log-hierarchy (#3)

---
+++ src/main/java/com/gorkem/parentpath/firstchildrenpath/FirstChildrenClass.java
@@ -0,0 +1,12 @@
+package com.gorkem.parentpath.firstchildrenpath;
+
+import org.slf4j.LoggerFactory;
+
+public class FirstChildrenClass implements Runnable {
+    private static final org.slf4j.Logger log = LoggerFactory.getLogger("src.main.java.com.gorkem.parentpath.firstchildrenpath");
+
+    @Override
+    public void run() {
+
+    }
+}
---
+++ src/main/java/com/gorkem/parentpath/secondchildrenpath/SecondChildrenClass.java
@@ -0,0 +1,12 @@
+package com.gorkem.parentpath.secondchildrenpath;
+
+import org.slf4j.LoggerFactory;
+
+public class SecondChildrenClass implements Runnable {
+    private static final org.slf4j.Logger log = LoggerFactory.getLogger("src.main.java.com.gorkem.parentpath.secondchildrenpath");
+
+    @Override
+    public void run() {
+
+    }
+}
