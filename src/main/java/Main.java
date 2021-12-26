a8c53d50fb559ebb2c8aa8058892ac3e376f445b
Görkem Bilim <gorkemblm@gmail.com>
2021-12-26 19:22:57 +0300

    added-executor-service-for-all-threads (#6)

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,8 +1,22 @@
+import com.gorkem.parentpath.firstchildrenpath.FirstChildrenClass;
 import com.gorkem.parentpath.httpserver.BasicHttpServer;
+import com.gorkem.parentpath.secondchildrenpath.SecondChildrenClass;
+
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
 
 public class Main {
+    private static final int poolSize = 3;
+
     public static void main(String[] args) {
         BasicHttpServer server = new BasicHttpServer();
-        server.run();
+        FirstChildrenClass firstChildrenClass = new FirstChildrenClass();
+        SecondChildrenClass secondChildrenClass = new SecondChildrenClass();
+
+        ExecutorService executorService = Executors.newFixedThreadPool(poolSize);
+
+        executorService.execute(server);
+        executorService.execute(firstChildrenClass);
+        executorService.execute(secondChildrenClass);
     }
 }
