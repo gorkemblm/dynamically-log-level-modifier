5553539c49ecb52331c871b2f47e753bdfb057cd
Görkem Bilim <gorkemblm@gmail.com>
2021-12-26 19:06:49 +0300

    general-refactor (#4)
    carried-class-for-global-scope
    added-logger-util-class

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,5 +1,8 @@
+import com.gorkem.parentpath.httpserver.BasicHttpServer;
+
 public class Main {
     public static void main(String[] args) {
-
+        BasicHttpServer server = new BasicHttpServer();
+        server.run();
     }
 }
---
+++ src/main/java/com/gorkem/parentpath/LoggerUtils.java
@@ -0,0 +1,24 @@
+package com.gorkem.parentpath;
+
+import org.slf4j.Logger;
+
+import java.util.Timer;
+import java.util.TimerTask;
+
+public class LoggerUtils {
+    public static void printLogPerSpecificTime(long sec, Logger log) {
+        Timer timer = new Timer();
+
+        TimerTask task = new TimerTask() {
+            @Override
+            public void run() {
+                if (log.isErrorEnabled()) { log.error("Logger name : {}. ERROR LOG", log.getName()); }
+                if (log.isWarnEnabled()) { log.warn("Logger name : {}. WARN LOG", log.getName()); }
+                if (log.isInfoEnabled()) { log.info("Logger name : {}. INFO LOG", log.getName()); }
+                if (log.isDebugEnabled()) { log.debug("Logger name : {}. DEBUG LOG", log.getName()); }
+                if (log.isTraceEnabled()) { log.trace("Logger name : {}. TRACE LOG", log.getName()); }
+            }
+        };
+        timer.scheduleAtFixedRate(task, 0, sec);
+    }
+}
